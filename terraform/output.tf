7b2cc57648d88592a8e658f44cc51ac9794e43eb
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2024-04-15 14:48:55 -0400

    Terraform output hostname
    Signed-off-by: Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,7 @@ output "cloudFrontDistributionID" {
 output "URL" {
   value = format("s3://%s?region=%s", module.cdn.s3_bucket, data.aws_region.current.name)
 }
+
+output "hostname" {
+  value = module.cdn.aliases[0]
+}
